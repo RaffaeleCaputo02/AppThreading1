bfaf452b7c7357bff6bf3ab7e17854a8a3077eaf
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-15 09:48:44 +0200

    argimo

--- AppThreading1/MainWindow.xaml.cs
+++ AppThreading1/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,51 @@ namespace AppThreading1
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Btn_Task_Click(object sender, RoutedEventArgs e)
+        {
+            //DoWork();
+            Task.Factory.StartNew(DoWork);
+
+        }
+
+        private void DoWork()
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                for (int j = 0; j <= 1000; j++)
+                {
+
+                }
+            }
+            Dispatcher.Invoke(AggiornamentoInterfaccia);
+        }
+
+        private void AggiornamentoInterfaccia()
+        {
+            lbl_risultato.Content = "Finito";
+        }
+
+        private void Btn_Conta_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Factory.StartNew(DoCount);
+        }
+
+        private void DoCount()
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                for (int j = 0; j <= 1000; j++)
+                {
+                    Dispatcher.Invoke(()=>AggiornamentoInterfaccia(j));
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+        private void AggiornamentoInterfaccia(int j)
+        {
+            lbl_Conta.Content = j.ToString();
         }
     }
 }
